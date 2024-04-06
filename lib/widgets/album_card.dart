b81da243d48f1e8d1f866d31deef0adb69cb2c1c
kenresoft <amadikenneth8@gmail.com>
2024-04-06 21:01:50 +0100

    Update album_card.dart

--- lib/widgets/album_card.dart
+++ lib/widgets/album_card.dart
@@ -1,24 +1,35 @@
 import 'package:extensionresoft/extensionresoft.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ost_music_app_ui/routes/now_playing.dart';
 
 import '../data/constants/constants.dart';
-import '../main.dart';
-import '../providers/providers.dart';
 import '../utils/utils.dart';
 
 class AlbumCard extends StatelessWidget {
-  const AlbumCard({required this.index, super.key, required this.ref});
+  const AlbumCard({
+    required this.index,
+    super.key,
+    /*required this.ref*/
+  });
 
   final int index;
-  final WidgetRef ref;
+
+  //final WidgetRef ref;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        launch(context, Constants.nowPlaying, Constants.albumList[index]);
-        ref.watch(currentlyPlaying.notifier).current(Constants.albumList[index]);
+        //launch(context, Constants.nowPlaying, Constants.albumList[index]);
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const NowPlaying(),
+            settings: RouteSettings(name: 'album', arguments: Constants.albumList[index]),
+          ),
+        );
+        //ref.watch(currentlyPlaying.notifier).current(Constants.albumList[index]);
       },
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,31 +37,31 @@ class AlbumCard extends StatelessWidget {
           Card(
             elevation: 2,
             shadowColor: const Color(0xff866684),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12).r),
             child: Container(
-              height: 110,
+              height: 110.h,
               width: Constants.wrapWidth,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
+                borderRadius: BorderRadius.circular(12).r,
                 color: Colors.black45,
                 image: DecorationImage(image: Utils.buildExactAssetImage(index), fit: BoxFit.cover),
               ),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 4),
+            padding: EdgeInsets.only(left: 4.w),
             child: Constants.wrapWidth.spaceX(Text(
               overflow: TextOverflow.ellipsis,
               Constants.albumList[index].title!,
-              style: TextStyle(fontSize: 16, color: Colors.white.withAlpha(190)),
+              style: TextStyle(fontSize: 16.h, color: Colors.white.withAlpha(190)),
             )),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 4),
+            padding: EdgeInsets.only(left: 4.w),
             child: Constants.wrapWidth.spaceX(Text(
               softWrap: true,
               Constants.albumList[index].artiste!.name,
-              style: TextStyle(fontSize: 12, color: Colors.white.withAlpha(100)),
+              style: TextStyle(fontSize: 12.h, color: Colors.white.withAlpha(100)),
             )),
           ),
         ],
